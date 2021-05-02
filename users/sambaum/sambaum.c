0a5190f3f59a4f88875e7202b7446a693fc4244c
Samuel Baumgartner <samuel.baumgartner@gmail.com>
2021-05-02 20:57:18 +0200

    add NK65

--- users/sambaum/sambaum.c
+++ users/sambaum/sambaum.c
@@ -289,11 +289,10 @@ void matrix_scan_user(void) {
 
 
 ////////////// SUSPENDED STATE BEGIN //////////////
-void suspend_power_down_user(void) {
-    rgb_matrix_set_suspend_state(true);
-}
-
-void suspend_wakeup_init_user(void) {
-    rgb_matrix_set_suspend_state(false);
-}
+//void suspend_power_down_user(void) {
+//    rgb_matrix_set_suspend_state(true);
+//}
+//void suspend_wakeup_init_user(void) {
+//    rgb_matrix_set_suspend_state(false);
+//}
 ////////////// SUSPENDED STATE END ////////////////
